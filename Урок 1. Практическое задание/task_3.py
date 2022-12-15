e2160c6f594f4387224c09e60c7454e3cba7b9d3
BogachevaKA <120360502+BogachevaKA@users.noreply.github.com>
2022-12-15 15:40:29 +0300

    Update task_3.py
    Задание 3 выполнено

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,11 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+Решение:
+n = int(input('Введите число n: '))
+temp = str(n)
+n2 = temp + temp
+n3 = temp + temp + temp
+result = n + int(n2) + int(n3)
+print('n + nn + nnn = ', result)
