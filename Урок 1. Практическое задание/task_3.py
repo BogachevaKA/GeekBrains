205dd7713da199e1bd65f3cc203a00bd6cba4e7c
BogachevaKA <120360502+BogachevaKA@users.noreply.github.com>
2022-12-27 09:24:03 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -11,8 +11,7 @@ n + nn + nnn = 369
 
 Решение:
 n = int(input('Введите число n: '))
-temp = str(n)
-n2 = temp + temp
-n3 = temp + temp + temp
-result = n + int(n2) + int(n3)
+n2 = n + n
+n3 = n + n +n
+result = str(n) + str(n2) + str(n3)
 print('n + nn + nnn = ', result)
